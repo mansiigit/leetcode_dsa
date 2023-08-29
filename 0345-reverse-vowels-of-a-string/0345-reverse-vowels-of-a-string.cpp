c77432196befb220c4bcc0efe3dc92d622d7673a
mansiigit <138381141+mansiigit@users.noreply.github.com>
2023-08-29 16:12:19 +0530

    Time: 9 ms (39.37%), Space: 7.7 MB (78.89%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j)
+        {
+         if(s[i]==' '|| (!isalpha(s[i])) )
+         {
+           i++;
+         }
+          if(s[j]==' ' || (!isalpha(s[j])))
+         {
+           j--;
+         }
+         else{
+         if(s[i]!= 'a'&&s[i]!='A' && s[i]!='e'&&s[i]!='E'&&s[i]!= 'i'&&s[i]!='I' &&s[i]!= 'o'&&s[i]!='O' &&s[i]!= 'u'&&s[i]!='U') 
+        {
+          i++;
+        }
+        
+        
+         if( s[j]!='a'&& s[j]!='A'&&s[j]!= 'e'&&s[j]!='E'&& s[j]!='i'&&s[j]!='I'&& s[j]!='o'&&s[j]!='O' &&s[j]!= 'u'&&s[j]!='U')
+        {
+          j--;
+        }
+        if((s[i]== 'a'||s[i]== 'A'|| s[i]=='e'||s[i]== 'E'||
+        s[i]== 'i' ||s[i]== 'I'||s[i]== 'o' ||s[i]== 'O'||s[i]== 'u'||
+        
+        s[i]== 'U') && (s[j]=='a'||s[j]== 'A'||s[j]== 'e'||s[j]== 'E'|| 
+        s[j]=='i' ||s[j]== 'I'|| s[j]=='o' ||s[j]== 'O'||
+        s[j]== 'u'||s[j]== 'U'))
+        {
+          swap(s[i],s[j]);
+          i++;
+          j--;
+        }
+         }
+        }
+         return s;
+    }
+  
+     
+    
+    
+    
+};
