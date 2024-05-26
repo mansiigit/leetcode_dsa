790e04db5f379a16a847a10f7ea10a245f82741e
Mansi Rai <138381141+mansiigit@users.noreply.github.com>
2024-05-27 01:58:57 +0530

    Time: 3 ms (43.78%), Space: 7.6 MB (22.21%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        
+        for (char ch : s) {
+            if (!st.empty() && ch == ')' && st.top() == '(') {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        
+       
+        return st.size();
+    }
+};
